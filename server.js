f5d78514a1882cd0593e9dde72a3144f16470c16
xylops <rankine1607@gmail.com>
2016-11-01 15:52:16 +0800

    Add update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-    if(req.headers['x-forwarded-proto'] = 'http'){
+    if(req.headers['x-forwarded-proto'] === 'http'){
         next();
     }else{
         res.redirect('http://' + req.hostname + req.url);
@@ -16,5 +16,5 @@ app.use(function(req, res, next){
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-    console.log('Express server is up on port' + PORT)
+    console.log('Express server is up on port ' + PORT)
 });
